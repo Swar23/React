10e9c9944b7b28b7430b00c7cff904586796e7fd
Swarup <swarupc@rocketmail.com>
2021-12-18 16:19:43 +0530

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+const text = "react";
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello {text}</h1> 
+    </div>
+  );
+}
+
+export default App;
